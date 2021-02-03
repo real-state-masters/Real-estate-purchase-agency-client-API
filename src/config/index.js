1a963ac1d20d02cf1f79e8097fe1b1566de7f5f1
katia <kcamivo90@gmail.com>
2021-02-03 16:46:47 +0100

    * Add firebase auth config #7

--- src/config/index.js
+++ src/config/index.js
@@ -8,13 +8,15 @@ const {
   MONGO_DB_URL_PRODUCTION,
   MONGO_DB_URL_DEVELOPMENT,
   MONGO_DB_URL_TEST,
-  PORT = 4000,
   JWT_SECRET,
   BCRYPT_SALT_ROUNDS,
 } = process.env;
 
 const baseConfig = {
-  port: PORT,
+  port: process.env.PORT || 4000,
+  client: {
+    URL: process.env.CLIENT_URL || "http://localhost:3000",
+  },
   jwt: {
     secret: JWT_SECRET,
   },
@@ -34,17 +36,62 @@ const config = {
     db: {
       url: MONGO_DB_URL_DEVELOPMENT,
     },
+    firebase: {
+      certConfig: {
+        type: process.env.FB_CERT_TYPE,
+        project_id: process.env.FB_CERT_PROJECT_ID,
+        private_key_id: process.env.FB_CERT_PRIVATE_KEY_ID,
+        private_key: process.env.FB_CERT_PRIVATE_KEY,
+        client_email: process.env.FB_CERT_CLIENT_EMAIL,
+        client_id: process.env.FB_CERT_CLIENT_ID,
+        auth_uri: process.env.FB_CERT_AUTH_URI,
+        token_uri: process.env.FB_CERT_TOKEN_URI,
+        auth_provider_x509_cert_url:
+          process.env.FB_CERT_AUTH_PROVIDER_X_509_CERT_URL,
+        client_x509_cert_url: process.env.FB_CERT_CLIENT_X_509_CERT_URL,
+      },
+    },
   },
   test: {
     ...baseConfig,
     db: {
       url: MONGO_DB_URL_TEST,
     },
+    firebase: {
+      certConfig: {
+        type: process.env.FB_CERT_TYPE,
+        project_id: process.env.FB_CERT_PROJECT_ID,
+        private_key_id: process.env.FB_CERT_PRIVATE_KEY_ID,
+        private_key: process.env.FB_CERT_PRIVATE_KEY,
+        client_email: process.env.FB_CERT_CLIENT_EMAIL,
+        client_id: process.env.FB_CERT_CLIENT_ID,
+        auth_uri: process.env.FB_CERT_AUTH_URI,
+        token_uri: process.env.FB_CERT_TOKEN_URI,
+        auth_provider_x509_cert_url:
+          process.env.FB_CERT_AUTH_PROVIDER_X_509_CERT_URL,
+        client_x509_cert_url: process.env.FB_CERT_CLIENT_X_509_CERT_URL,
+      },
+    },
   },
   production: {
     ...baseConfig,
     db: {
       url: MONGO_DB_URL_PRODUCTION,
+    },
+    firebase: {
+      certConfig: {
+        type: process.env.FB_CERT_TYPE,
+        project_id: process.env.FB_CERT_PROJECT_ID,
+        private_key_id: process.env.FB_CERT_PRIVATE_KEY_ID,
+        private_key: process.env.FB_CERT_PRIVATE_KEY,
+        client_email: process.env.FB_CERT_CLIENT_EMAIL,
+        client_id: process.env.FB_CERT_CLIENT_ID,
+        auth_uri: process.env.FB_CERT_AUTH_URI,
+        token_uri: process.env.FB_CERT_TOKEN_URI,
+        auth_provider_x509_cert_url:
+          process.env.FB_CERT_AUTH_PROVIDER_X_509_CERT_URL,
+        client_x509_cert_url: process.env.FB_CERT_CLIENT_X_509_CERT_URL,
+      },
     },
   },
 };
